f6a30219d6ba1a85dbe566ab35c79c3522bd4722
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-07-26 11:05:55 +0530

    Interaction with the GitHub API

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+    constructor(){
+        this.client_id = Can't be disclosed ;
+        this.client_secret = Can't be disclosed ;
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch
+        (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const repoResponse = await fetch
+        (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+
+        return {
+            // If the variable is profile then "profile: profile" and "simply profile" mean the same thing 
+            profile,
+            repos
+        }
+    }
+}
